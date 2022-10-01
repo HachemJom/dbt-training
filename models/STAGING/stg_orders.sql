781fa4bc4373c79acb6882d98d7a41409ce01caf
hachem.jomni@hec.ca <hachem.jomni@hec.ca>
2022-10-01 15:14:25 +0000

    developing stg_orders

---
+++ models/STAGING/stg_orders.sql
@@ -0,0 +1,20 @@
+select
+    --from raw orders
+    O.orderid,
+    O.orderdate,
+    O.shipdate,
+    O.shipmode,
+    O.ordersellingprice - O.ordercostprice as orderprofit,
+    O.ordercostprice,
+    O.ordersellingprice,
+    --from raw customer
+    C.customername,
+    C.segment,
+    C.country,
+    --from raw produc
+    P.category,
+    P.productname,
+    P.subcategory
+from {{ ref('raw_orders') }} as O
+left join {{ ref('raw_customer') }} as C on C.customerid=O.customerid
+left join {{ ref('raw_product') }} as P on P.productid=O.productid
