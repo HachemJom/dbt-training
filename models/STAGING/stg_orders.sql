6c5de8f73b1d5caf37caf3c46e02c9fbe7510925
hachem.jomni@hec.ca <hachem.jomni@hec.ca>
2022-10-02 19:48:25 +0000

    added delivery seed team and edit stage orders

--- models/STAGING/stg_orders.sql
+++ models/STAGING/stg_orders.sql
@@ -1,5 +1,6 @@
 select
     --from raw orders
+    {{ dbt_utils.surrogate_key(['O.orderid', 'C.customerid', 'P.productid']) }} as sk_orders,
     O.orderid,
     O.orderdate,
     O.shipdate,
@@ -17,7 +18,9 @@ select
     P.category,
     P.productname,
     P.subcategory,
-    {{ markup('ordersellingprice','ordercostprice') }} as markup
+    {{ markup('ordersellingprice','ordercostprice') }} as markup,
+    D.DELIVERY_TEAM
 from {{ ref('raw_orders') }} as O
 left join {{ ref('raw_customer') }} as C on C.customerid=O.customerid
 left join {{ ref('raw_product') }} as P on P.productid=O.productid
+left join {{ ref('delivery_team') }} as D on D.shipmode=O.shipmode
